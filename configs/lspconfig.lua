7ab99bd5eddc7a00545e84674ad1ff28ff36278e
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-07-11 22:06:54 +0200

    Initial commit with rust config

---
+++ configs/lspconfig.lua
@@ -0,0 +1,5 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local lspconfig = require("lspconfig")
+local lspconfig_util = require("lspconfig/util")
+
